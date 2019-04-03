ff635ce48c0ed1cacafe770009054a3d1c2cd15c
Cayla Ramirez <caylaxnicole@gmail.com>
2019-04-03 13:49:32 -0700

    Added logo and changed images on second slide

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Root, Routes } from 'react-static'
 import { Link } from '@reach/router'
 import 'bootstrap/dist/css/bootstrap.css';
-
+import logo from 'img/icon-sprout.png'
 import './app.css'
 
 function App() {
   return (
     <Root>
       <nav>
-        <div> Plant bb</div>
+        <div> Plant bb
+          <img className='logo' src={logo} alt='plant logo'/>
+        </div>
         <Link to="/">Home</Link>
         <Link to="/sign-up">Sign-up</Link>
         <Link to="/blog">Sign-in</Link>
--- client-side/src/pages/index.js
+++ client-side/src/pages/index.js
@@ -3,13 +3,12 @@ import { withSiteData } from 'react-static'
 import Carousel from 'react-bootstrap/Carousel'
 import Button from 'react-bootstrap/Button'
 import plant1 from '../img/plant1.jpeg'
-import plantMan from '../img/man-watering-tree.jpg'
-import plantLady from '../img/woman-watering-tree.jpg'
+import plantNursery from '../img/Plant-Nursery-Vector.jpg'
 
 export default withSiteData(() => (
   <div style={{ textAlign: 'center' }}>
     <div className='bg-img'>
-      <h1>Welcome to Plant bb!</h1>
+      <h1>Plant bb</h1>
       <div className='hero-image'>
       <Carousel>
         <Carousel.Item className='c-slides'>
@@ -24,10 +23,9 @@ export default withSiteData(() => (
         </Carousel.Item>
         <Carousel.Item className='c-slides'>
           <Carousel.Caption>
-            <img src={plantMan} alt='guy watering plant'/>
-            <img src={plantLady} alt='woman watering plant'/>
+            <img src={plantNursery} alt='guy watering plant'/>
             <h3>Get email reminders!</h3>
-            <p>Set up an automatic email blast as a reminder to water your plant bbs at home..</p>
+            <p>Set up an automatic email blast as a reminder to water your plant bbs at home.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
